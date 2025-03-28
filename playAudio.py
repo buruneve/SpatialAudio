c72b614c5907a2f2eb88efeb35b00b85f1cd96d7
buruneve <burunova5@gmail.com>
2025-03-28 11:30:37 -0700

    updated code

--- playAudio.py
+++ playAudio.py
@@ -1,41 +1,44 @@
 #playAudio.py
 
-## # %%
-
 #imports
 import sounddevice as sd
 import numpy as np
 
-#settings
-# all 4 speakers: [1,2,3,4] [LF, RF, LB, RB]
-# front two: [1,2]; back two: [3,4]
 
-userInput = input('Which speakers do you want to play? \nEnter values separated by space: ')
-userInput = userInput.split()
-whichSpeakers = np.array(userInput, dtype=int) 
+# PROMPT USER + SETTINGS
+# - total number of speakers
+# - to determine which speakers to play sound from 
+#       all 4 speakers: [1,2,3,4] [LF, RF, LB, RB]
+#       front two: [1,2]; back two: [3,4]
+# - which frequencies to play
 
+userInputNumSpeakers = input('Total number of speakers?: ')
+numSpeakers = np.arange(1, int(userInputNumSpeakers)+1)
 
+userInputSpeakers = input('Which speakers to play sound from? Enter values separated by space: ')
+userInputSpeakers = userInputSpeakers.split()
+whichSpeakers = np.array(userInputSpeakers, dtype=int) 
+
+userInputFreq = input('Frequency values? Enter values separated by space: ')
+userInputFreq = userInputFreq.split()
+freq = np.array(userInputFreq, dtype=int) 
+
+# settings
 fs = 44100
 sd.default.samplerate = fs
-sd.default.channels = 4
-
-numSpeakers = np.array([1,2,3,4]) # total number of speakers
-f = np.array([200, 300]) #, 400, 500, 600])
+sd.default.channels = userInputNumSpeakers
 t = np.arange(0, 5, 1/44100)
-print(f); print(len(t)); #print(np.shape(t))
+
 
 # view device info
-getDeviceInfo = sd.query_devices(kind = 'output')
-#print(dict(getDeviceInfo)) #dictionary (key, value)
-
-x = getDeviceInfo["index"] # device ID 
-#print(x)
+getDeviceInfo = sd.query_devices(kind = 'output')  #print(dict(getDeviceInfo)) #dictionary (key, value)
+x = getDeviceInfo["index"] # device ID  #print(x)
 
 
 # play sound 
-indx=0 # indexing starts at 0
-for idxF in f:
-    xt = np.sin(2*np.pi*f[indx]*t) 
+indx=0  #indexing starts at 0
+for idxF in freq:
+    xt = np.sin(2*np.pi*freq[indx]*t) 
     indx = indx+1
 
     for spkr in whichSpeakers:
