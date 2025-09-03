5899195917d1ec666df29cbeeb7463e2e389654e
buruneve <burunova5@gmail.com>
2025-09-03 12:31:31 -0700

    add px4 comm file

--- px4_comm.py
+++ px4_comm.py
@@ -1,53 +1,79 @@
-#px4_comm.py
-
-"""
-Example: Set that a message is streamed at particular rate
-"""
+# px4_comm.py
+# communicate with px4: send/receiveve messages
 
 from pymavlink import mavutil
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+mavutil.set_dialect("development")
 
-# Start a connection listening on a UDP port
-connection = mavutil.mavlink_connection(device='COM6')
+# Start a listening connection
+the_connection = mavutil.mavlink_connection(device='COM5', baud=11500) #, dialect = 'custom'
 
-# Wait for the first heartbeat to set the system and component ID of remote system for the link
-connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" % (connection.target_system, connection.target_component))
-
-
-# response = connection.recv_match(blocking=True)
-# print(response)
-# print(f"[{response.get_type()}] {response.to_dict()}")
-
-
-while True:
-    msg = connection.recv_match(blocking=True) #recv_match() waits for the next available mavlink message, parses it, & returns a python object representing the message 
-    print(msg)
-#     print(f"[{msg.get_type()}] {msg.to_dict()}")
+# # Wait for the first heartbeat
+# #   This sets the system and component ID of remote system for the link
+the_connection.wait_heartbeat()  
+print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
 
-# # Define command_long_encode message to send MAV_CMD_SET_MESSAGE_INTERVAL command
-# # param1: MAVLINK_MSG_ID_BATTERY_STATUS (message to stream)
-# # param2: 1000000 (Stream interval in microseconds)
-# message = connection.mav.command_long_encode(
-#         connection.target_system,  # Target system ID
-#         connection.target_component,  # Target component ID
-#         mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send
-#         0,  # Confirmation
-#         mavutil.mavlink.MAVLINK_MSG_ID_BATTERY_STATUS,  # param1: Message ID to be streamed
-#         1000000, # param2: Interval in microseconds
-#         0,       # param3 (unused)
-#         0,       # param4 (unused)
-#         0,       # param5 (unused)
-#         0,       # param5 (unused)
-#         0        # param6 (unused)
-#         )
+#MAV_CMD_MISSION_START
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, 
+                                     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,0,0,0,0,0,0,0,0)
+
+#the_connection.mav.send(message)
+msg = the_connection.recv_match(type='COMMAND_ACK',blocking=True)  # acknowledge command 
+print(msg) 
+
+# sensor_avs message ID
+message_id = 292 
+print("message_id: ",message_id)
+
+message = the_connection.mav.command_long_encode(    #encode
+        the_connection.target_system,  # Target system ID
+        the_connection.target_component,  # Target component ID
+        mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send  #MAV_CMD_REQUEST_MESSAGE
+        0,  # Confirmation
+        message_id, #mavutil.mavlink.MAVLINK_MSG_ID_SENSOR_AVS  # param1: Message ID to be streamed
+        50000, # param2: Interval in microseconds 1000000
+        0,0,0,0,0)
+print(message)
 
 # # Send the COMMAND_LONG
-# connection.mav.send(message)
+the_connection.mav.send(message)
+msg2 = the_connection.recv_match(type='COMMAND_ACK',blocking=True)  # acknowledge command 
+print(msg2) 
+print('')
 
-# # Wait for a response (blocking) to the MAV_CMD_SET_MESSAGE_INTERVAL command and print result
-# response = connection.recv_match(type='COMMAND_ACK', blocking=True)
-# if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
-#     print("Command accepted")
-# else:
-#     print("Command failed")
+# extract: azimuth deg 
+# tresholds 
+# active intensity - how loud sound is 
+# q_factor - diffusion/concentration of sound 
+# plot - mel intesity
+# spectrograms 
+while True:   #COMMAND_ACK  #UNKNOWN_292 type='SENSOR_AVS', 
+        res = the_connection.recv_match(blocking=True)  # receives all the messages available 
+        print(res) # get a lot of data 
+        
+        azimuth_deg = the_connection.messages['SENSOR_AVS'].azimuth_deg
+        print("azimuth_deg: ",azimuth_deg)
+        
+        q_factor = the_connection.messages['SENSOR_AVS'].q_factor
+        print("q_factor: ",q_factor)
+        
+        mel_intensity = the_connection.messages['SENSOR_AVS'].mel_intensity
+        print("mel_intesity: ",mel_intensity)
+        print('')
+
+        time_boot_ms  = the_connection.messages['ATTITUDE'].time_boot_ms 
+        print("time_boot_ms: ",time_boot_ms )
+
+        yaw = the_connection.messages['ATTITUDE'].yaw  #GUI 
+        print("yaw: ",yaw)
+
+        pitch = the_connection.messages['ATTITUDE'].pitch
+        print("pitch: ", pitch)
+
+        roll = the_connection.messages['ATTITUDE'].roll
+        print("roll: ", roll)
+        print()
+        
